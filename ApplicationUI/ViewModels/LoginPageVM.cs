e9a364fa45759d77820ebe101c4a137ceb69778f
Khajiitspy <150380340+Khajiitspy@users.noreply.github.com>
2025-02-06 18:33:08 -0500

    Added password encryption and decryption

--- ApplicationUI/ViewModels/LoginPageVM.cs
+++ ApplicationUI/ViewModels/LoginPageVM.cs
@@ -40,7 +40,7 @@ namespace ApplicationUI.ViewModels
                 var users = _userService.GetAll();
                 foreach (var user in users)
                 {
-                    if (Password == user.Password && user.Nickname == Nickname)
+                    if (BCrypt.Net.BCrypt.Verify(Password,user.Password) && user.Nickname == Nickname)
                     {
                         StaticUser.User = user;
                         StaticUser.IsLoggedIn = true;
