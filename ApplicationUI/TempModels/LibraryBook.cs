f7cd80c01b24ad12b72969d55328aec0eed0323a
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-02-08 10:45:50 +0200

    Added LibraryBook class for saving data about book, so user can pick one to download into personal library

---
+++ ApplicationUI/TempModels/LibraryBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationUI.TempModels
+{
+    public class LibraryBook
+    {
+        public int Id { get; set; } // щоби після обрання в ListBox певної книги для закачування користувачем, можна було легше дізнатися яку саме закачувати
+        public string Name { get; set; } // Назва книги
+        public string Author { get; set; } // Автор (вся string, яка відноситься до Author)
+        public string FilePath { get; set; } // Шлях до завантаженого файлу на пристрої
+        public byte[] Cover { get; set; } // url посилання на фото буде вже на самій сторінці, коли переходиться на певну книгу)
+        public string EpubSource { get; set; } // це потрібне лише для Selenium, щоби зберегти посилання, за яким потрібно перейти для самого закачування, в цьому випадку, коли натискається картинка Epub)
+    }
+}
