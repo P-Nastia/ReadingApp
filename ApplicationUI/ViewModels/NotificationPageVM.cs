491cbcd110a40e24b94621b6ef1b681748880719
Khajiitspy <150380340+Khajiitspy@users.noreply.github.com>
2025-02-08 12:31:47 -0500

    Constructor for NotificationPageVM

--- ApplicationUI/ViewModels/NotificationPageVM.cs
+++ ApplicationUI/ViewModels/NotificationPageVM.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using BLL.Interfaces;
+using BLL.ModelsDTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +13,18 @@ namespace ApplicationUI.ViewModels
     public class NotificationPageVM : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+        private IUserService<BookDTO, UserDTO> _userService;
+
+        public void OnNotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public NotificationPageVM(IUserService<BookDTO, UserDTO> userService)
+        {
+            _userService = userService;
+        }
     }
 }
