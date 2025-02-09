5dc1b0f13eac23c0f032cf5760dfe44c0aed0153
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-02-09 13:32:42 +0200

    commited to change the branch

--- ApplicationUI/ViewModels/AllBooksPageVM.cs
+++ ApplicationUI/ViewModels/AllBooksPageVM.cs
@@ -102,10 +102,10 @@ namespace ApplicationUI.ViewModels
             {
                 LibraryBook libraryBook = new LibraryBook()
                 {
-                    Name = "Girl Online Going Solo",
-                    Author = "Zoe Sugg",
-                    FilePath = "D:\\Викачування\\_OceanofPDF.com_Going_Solo_-_Zoe_Sugg.epub",
-                    Cover = File.ReadAllBytes("D:\\Викачування\\PDF-EPUB-Girl-Online-Going-Solo-Download.jpg")
+                    Name = "Lessons In Faking",
+                    Author = "Selina Mae",
+                    FilePath = "D:\\Викачування\\_OceanofPDF.com_Lessons_in_Faking_-_Selina_Mae.epub",
+                    Cover = File.ReadAllBytes("D:\\Викачування\\PDF-EPUB-Lessons-In-Faking-by-Selina-Mae-Download.jpg")
                 };
                 bookFile = EpubReader.ReadBook(libraryBook.FilePath);
                 BookDTO book = new BookDTO()
@@ -145,117 +145,5 @@ namespace ApplicationUI.ViewModels
                 _userService.AddBook(StaticUser.User, bookFromDB);
             });
         }
-        //private async void Download()
-        //{
-        //    if (CanDownload)
-        //    {
-        //        await Task.Run(() =>
-        //        {
-        //            Response = String.Empty;
-        //            OnNotifyPropertyChanged("Response");
-        //            ChromeOptions options = new ChromeOptions();
-        //            downloadDirectory = Directory.GetCurrentDirectory() + $"\\Files";
-        //            Directory.CreateDirectory(downloadDirectory);
-
-        //            options.AddArgument("--headless");
-        //            options.AddUserProfilePreference("download.default_directory", downloadDirectory);
-        //            options.AddUserProfilePreference("plugins.always_open_pdf_externally", true);
-        //            options.AddUserProfilePreference("download.prompt_for_download", false);
-
-        //            var driverService = ChromeDriverService.CreateDefaultService();
-        //            driverService.HideCommandPromptWindow = true;
-
-        //            bool isDownloaded = false;
-        //            using (IWebDriver driver = new ChromeDriver(driverService, options))
-        //            {
-
-        //                driver.Navigate().GoToUrl(href);
-
-        //                var pdfSources = driver.FindElements(By.XPath("//*[@id=\"mm-0\"]/div[2]/div/div[6]/div[3]/a[6]"));
-
-        //                if (pdfSources.Any())
-        //                {
-        //                    var info = driver.FindElement(By.XPath("//*[@id=\"mm-0\"]/div[2]/div/div[4]/h2")).GetAttribute("innerText").Split(',');
-        //                    var book = _bookService.GetByNameAndAuthor(info[1], info[0]);
-
-        //                    if (book == null)
-        //                    {
-        //                        pdfSources.First().Click();
-        //                        Thread.Sleep(4000);
-        //                        Actions actions = new Actions(driver);
-        //                        actions.MoveByOffset(500, 500).Click().Perform();
-        //                        var filesFromDirectory = Directory.GetFiles(downloadDirectory);
-
-        //                        string pdfPath = filesFromDirectory.Where(f => f.Contains(".pdf")).FirstOrDefault();
-        //                        string text = "";
-
-        //                        //try
-        //                        //{
-        //                        //    using (PdfReader reader = new PdfReader(pdfPath))
-        //                        //    using (PdfDocument pdfDoc = new PdfDocument(reader))
-        //                        //    {
-        //                        //        for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
-        //                        //        {
-        //                        //            var page = pdfDoc.GetPage(i);
-        //                        //            var strategy = new LocationTextExtractionStrategy();
-        //                        //            var textFromPage = PdfTextExtractor.GetTextFromPage(page, strategy);
-        //                        //            text += textFromPage;
-        //                        //        }
-
-        //                        //        List<string> paragraphs = text.Split(new[] { ".\n" }, StringSplitOptions.None).ToList();
-
-        //                        //        BookDTO tempBook = new BookDTO()
-        //                        //        {
-        //                        //            Author = info[0],
-        //                        //            Name = info[1],
-        //                        //            Users = new List<UserDTO>()
-        //                        //        };
-        //                        //        List<ParagraphDTO> p = new List<ParagraphDTO>();
-        //                        //        foreach (var par in paragraphs)
-        //                        //        {
-        //                        //            p.Add(new ParagraphDTO() { Text = par, Book = tempBook, UserComments = new List<UserCommentDTO>() });
-        //                        //        }
-        //                        //        tempBook.Paragraphs = p;
-        //                        //        _bookService.AddBook(tempBook);
-        //                        //        var bookFromDB = _bookService.GetByNameAndAuthor(tempBook.Name, tempBook.Author);
-        //                        //        _userService.AddBook(StaticUser.User, bookFromDB);
-
-        //                        //        SearchString = String.Empty;
-        //                        //        OnNotifyPropertyChanged("SearchString");
-        //                        //        MessageBox.Show("Book added to your library", "Download completed", MessageBoxButton.OK, MessageBoxImage.Information);
-        //                        //        isDownloaded = true;
-        //                        //    }
-        //                        //}
-        //                        //catch (Exception ex)
-        //                        //{
-        //                        //    MessageBox.Show($"Error: {ex.Message}");
-        //                        //}
-        //                    }
-        //                    else
-        //                    {
-        //                        _userService.AddBook(StaticUser.User, book);
-        //                        isDownloaded = true;
-        //                        MessageBox.Show("Book added to your library", "Download completed", MessageBoxButton.OK, MessageBoxImage.Information);
-        //                    }
-        //                }
-        //                driver.Quit();
-
-        //            }
-        //            var files = Directory.GetFiles(downloadDirectory);
-        //            foreach (var file in files)
-        //            {
-        //                File.Delete(file);
-        //            }
-        //            Directory.Delete(downloadDirectory);
-        //            if (isDownloaded == false)
-        //            {
-        //                MessageBox.Show("Impossible to download book to your library", "Download error", MessageBoxButton.OK, MessageBoxImage.Information);
-        //            }
-        //            CanDownload = false;
-        //            OnNotifyPropertyChanged("CanDownload");
-        //        });
-        //    }
-            
-        //}
     }
 }
