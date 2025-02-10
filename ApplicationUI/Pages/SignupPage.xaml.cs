5197096e5b18f3be1c6380257b6d1875ec5374b2
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-02-10 23:15:13 +0200

    Add return button and add validation for password

--- ApplicationUI/Pages/SignupPage.xaml.cs
+++ ApplicationUI/Pages/SignupPage.xaml.cs
@@ -2,6 +2,8 @@
 using ApplicationUI.ViewModels;
 using ApplicationUI.Windows;
 using System.IO;
+using System.ServiceModel.Channels;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
@@ -22,12 +24,13 @@ namespace ApplicationUI.Pages
             // Setting images/Icons
             string CD = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\Images\\"; // Maybe there is  a better way to get the project directory...
             SignUpImage.Source = new BitmapImage(new Uri($"{CD}signUp.png", UriKind.Absolute));
+            ReturnImage.Source = new BitmapImage(new Uri($"{CD}Return.jpg", UriKind.Absolute));
             #endregion
 
             _signupPageVM = signupPageVM;
             this.DataContext = _signupPageVM;
         }
-        private void signUpClick(object sender, MouseButtonEventArgs e)
+        private  void signUpClick(object sender, MouseButtonEventArgs e)
         {
             SoundPlayer.PlayButtonSound();
             _signupPageVM.Password = this.passwordPB.Password;
@@ -37,14 +40,40 @@ namespace ApplicationUI.Pages
                 verificationWindow.ShowDialog();
                 if (verificationWindow.isVerificated)
                 {
-                    _signupPageVM.SignUp();
+                     _signupPageVM.SignUp();
                 }
+            }
+            else if(!_signupPageVM.IsNicknameValid())
+            {
+                MessageBox.Show("Wrong nickname", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (!_signupPageVM.IsPasswordValid())
+            {
+                MessageBox.Show("Wrong password", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (!_signupPageVM.IsEmailValid())
+            {
+                MessageBox.Show("Wrong email", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (!_signupPageVM.IsPhoneValid())
+            {
+                MessageBox.Show("Wrong phone", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (!_signupPageVM.IsIconValid())
+            {
+                MessageBox.Show("Wrong icon", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
         private void TextBox_TextInput(object sender, TextCompositionEventArgs e)
         {
 
+        }
+
+        private void ReturnImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            SoundPlayer.PlayButtonSound();
+            _signupPageVM.ReturnToSignIn();
         }
     }
 }
