b4cfe1964c1e7ed3b6a0371c05f7be2d1778fd2a
0Taras0 <rringoo043@gmail.com>
2025-02-13 18:02:28 +0200

    .

--- BLL/ModelsDTO/BookDTO.cs
+++ BLL/ModelsDTO/BookDTO.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
+        public List<ChapterDTO> Chapters { get; set; }
         public List<ParagraphDTO> Paragraphs { get; set; }
         public ICollection<UserDTO> Users { get; set; }
         public string DisplayBook => $"{Author}, {Name}";
--- DAL/Repositories/UserRepository.cs
+++ DAL/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DAL.Entities;
+using DAL.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
